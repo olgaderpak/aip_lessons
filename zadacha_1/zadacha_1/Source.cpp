da4f149af9aee3fab12bfe2d8080682bef027a87
olgaderpak <olgaderpak@mail.ru>
2021-02-26 21:44:16 +0300

    dz

---
+++ zadacha_1/zadacha_1/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "  ***" << std::endl
+		<< " *   *" << std::endl
+		<< "*     *" << std::endl
+		<< "*     *" << std::endl
+		<< " *   *" << std::endl
+		<< "  ***" << std::endl;
+
+	std::getchar();
+	std::getchar();
+
+	return 0;
+}
